493f507434d4109f5a600e40b594c30c2bff1e4b
Gus Wadas <61980392+gusWadas@users.noreply.github.com>
2023-06-17 17:01:36 -0300

    Navbar as canvas

--- arbo/src/shared/AppHeader.tsx
+++ arbo/src/shared/AppHeader.tsx
@@ -4,6 +4,10 @@ import { UserContext } from '../Context';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTree } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGrip } from '@fortawesome/free-solid-svg-icons';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { useWindowResize } from '../utils/customHooks'
 import { Link } from 'react-router-dom';
 import './AppHeader.css';
@@ -11,10 +15,26 @@ import './AppHeader.css';
 const sideBar = () => {
     return (
         <ul>
-            <li><Link to='login'>Login</Link></li>
-            <li><Link to='about'>Sobre</Link></li>
-            <li><Link to='signup'>Cadastrar</Link></li>
-            <li><Link to='contatos'>Contatos</Link></li>
+            <li><Link to='user'>
+                <p className="li_fa"><FontAwesomeIcon icon={faUser} id="icon_user"/></p>
+                <p className="li_text">  Nome Completo</p></Link>
+            </li>
+            <li><Link to='about'>
+                <p className="li_fa"><FontAwesomeIcon icon={faGrip} id="icon_dash"/></p>
+                <p className="li_text">  Dashboard</p></Link>
+            </li>
+            <li><Link to='about'>
+                <p className="li_fa"><FontAwesomeIcon icon={faBook} id="icon_inventory"/></p>
+                <p className="li_text">  Inventório Arbóreo</p></Link>
+            </li>
+            <li><Link to='about'>
+                <p className="li_fa"><FontAwesomeIcon icon={faTree} id="icon_tree"/></p>
+                <p className="li_text">  Cadastrar Árvore</p></Link>
+            </li>
+            <li><Link to='about'>
+                <p className="li_fa"><FontAwesomeIcon icon={faArrowRightFromBracket} id="icon_leave"/></p>
+                <p className="li_text">  Sair</p></Link>
+            </li>
         </ul>
     );
 }
